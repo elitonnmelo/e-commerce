b6b024fc9d748578c7297bdc76723183ea31842a
elitonnmelo <elitonp.melo@gmail.com>
2021-11-13 14:14:59 -0300

    adição das paginas login, recuperar senha, cadatro e home

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -16,7 +15,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -28,7 +26,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
